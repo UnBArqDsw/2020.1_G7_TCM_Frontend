0758dbbad20b6f43ee219e5981979a65932ebb82
João Soares <jpcirqueira81@gmail.com>
2020-10-05 15:40:56 -0300

    Revert "[#49] Adicionado componente de profile."
    This reverts commit 8f3d17f5e9d76be92f9010cbc1ce47d187987362.

--- tcm_frontend/src/routes.js
+++ tcm_frontend/src/routes.js
@@ -3,14 +3,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Screens/User/Account/Login';
 import SignUp from './Screens/User/Account/SignUp';
-import Profile from './Screens/Profile';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Login} />
       <Route path="/signup" component={SignUp} />
-      <Route path="/profile" component={Profile} />
     </Switch>
   );
 };
