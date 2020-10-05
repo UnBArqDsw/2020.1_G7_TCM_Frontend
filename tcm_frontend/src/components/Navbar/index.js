017b4bbc8b59b1143a0780c868be819bc3b38324
RCristyan <rcristyan9@gmail.com>
2020-10-05 20:33:42 -0300

    Mais ajustes no css da navbar

--- tcm_frontend/src/components/Navbar/index.js
+++ tcm_frontend/src/components/Navbar/index.js
@@ -16,25 +16,35 @@ function Navbar() {
       <BottomNavigationAction
         className="bottom-navigation-icon"
         name="feed"
-        label={<InputLabel className={styles.label}>Feed</InputLabel>}
+        label={<InputLabel className={styles.label_feed}>Feed</InputLabel>}
         icon={<Home className={styles.icon} />}
       />
       <BottomNavigationAction
         className="bottom-navigation-icon"
         name="search"
-        label={<InputLabel className={styles.label}>Pesquisa</InputLabel>}
+        label={
+          <InputLabel className={styles.label_search}>Pesquisa</InputLabel>
+        }
         icon={<Search className={styles.icon} />}
       />
       <BottomNavigationAction
         className="bottom-navigation-icon"
         name="create-tournament"
-        label={<InputLabel className={styles.label}>Criar Torneio</InputLabel>}
+        label={(
+          <InputLabel className={styles.label_create_tournament}>
+            Criar Torneio
+          </InputLabel>
+        )}
         icon={<AddCircle className={styles.icon} />}
       />
       <BottomNavigationAction
         className="bottom-navigation-icon"
         name="my-tournaments"
-        label={<InputLabel className={styles.label}>Meus Torneios</InputLabel>}
+        label={
+          <InputLabel className={styles.label_my_tournaments}>
+            Meus Torneios
+          </InputLabel>
+        }
         icon={<EmojiEvents className={styles.icon} />}
       />
     </BottomNavigation>
--- tcm_frontend/src/components/Navbar/style.js
+++ tcm_frontend/src/components/Navbar/style.js
@@ -8,14 +8,67 @@ export const makestyles = makeStyles({
     margin: '0',
   },
 
-  label: {
+  label_feed: {
     color: '#000000',
+    fontFamily: 'Roboto',
+    fontSize: '12px',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: '12px',
+    letterSpacing: '0em',
+    textAlign: 'center',
+    position: 'absolute',
+    bottom: '0px',
+    left: '30px',
+  },
+
+  label_search: {
+    color: '#000000',
+    fontFamily: 'Roboto',
+    fontSize: '12px',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: '12px',
+    letterSpacing: '0em',
+    textAlign: 'center',
+    position: 'absolute',
+    bottom: '0px',
+    left: '20px',
+  },
+
+  label_create_tournament: {
+    color: '#000000',
+    fontFamily: 'Roboto',
+    fontSize: '12px',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: '12px',
+    letterSpacing: '0em',
+    textAlign: 'center',
+    position: 'absolute',
+    bottom: '0px',
+    left: '10px',
+  },
+
+  label_my_tournaments: {
+    color: '#000000',
+    fontFamily: 'Roboto',
+    fontSize: '12px',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: '12px',
+    letterSpacing: '0em',
+    textAlign: 'center',
+    position: 'absolute',
+    bottom: '0px',
+    left: '0px',
   },
 
   icon: {
     color: '#050505',
     width: '28px',
     height: '32px',
+    position: 'absolute',
     opacity: '70%',
   },
 });
