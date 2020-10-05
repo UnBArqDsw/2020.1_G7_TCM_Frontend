f8922af5fdf785b98b22acc16030a265e6cabd52
lucas <lucas.alexandre.df@gmail.com>
2020-10-05 13:56:30 -0300

    [#50]Criado base da tela feed
    Signed-off-by: lucas <lucas.alexandre.df@gmail.com>

---
+++ tcm_frontend/src/Screens/Feed/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import AppBar from '../../components/Appbar';
+import NavBar from '../../components/Navbar';
+import { useStyles } from './styles';
+
+export default function Feed() {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container}>
+      <AppBar />
+      <div className={classes.footer}>
+        <NavBar />
+      </div>
+    </Container>
+  );
+}
--- tcm_frontend/src/routes.js
+++ tcm_frontend/src/routes.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Screens/User/Account/Login';
 import SignUp from './Screens/User/Account/SignUp';
-
-
+import Feed from './Screens/Feed';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Login} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/feed" component={Feed} />
     </Switch>
   );
 };
