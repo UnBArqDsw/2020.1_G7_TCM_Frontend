842026cd176f7ef4eaa33ca60695d6676493b444
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-10-05 19:48:27 -0300

    [#52]-Ajustes

--- tcm_frontend/src/Screens/Tournament/RegisterII/index.js
+++ tcm_frontend/src/Screens/Tournament/RegisterII/index.js
@@ -12,8 +12,7 @@ export const useStyles = makeStyles((theme) => ({
         width: '100%',
         margin: '0',
         padding: '0',
-        justify:"flex-start",
-        alignItems:"center"
+        justify:"flex-start", alignItems:"center"
 
         // display: 'block',
         // paddingRight: '0',
--- tcm_frontend/src/routes.js
+++ tcm_frontend/src/routes.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Screens/User/Account/Login';
 import SignUp from './Screens/User/Account/SignUp';
+import RegisterII from './Screens/Tournament/RegisterII';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Login} />
+      <Route exact path="/tournament2" component={RegisterII} />
       <Route path="/signup" component={SignUp} />
     </Switch>
   );
