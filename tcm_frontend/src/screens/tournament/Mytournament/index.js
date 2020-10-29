9c6bf97e0c8dedb17d1c1a39ef2bd537ffc86dbc
João Soares <jpcirqueira81@gmail.com>
2020-10-29 19:38:22 -0300

    adicionado card a tela de meus torneios.

--- tcm_frontend/src/screens/tournament/Mytournament/index.js
+++ tcm_frontend/src/screens/tournament/Mytournament/index.js
@@ -5,6 +5,7 @@ import Tab from '@material-ui/core/Tab';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '../../../components/Appbar';
 import NavBar from '../../../components/Navbar';
+import Card from '../../../components/Card';
 
 const MyTournament = () => {
   const handleChange = (event, newValue) => {
@@ -15,16 +16,16 @@ const MyTournament = () => {
     <Container>
       <AppBar />
       <Paper>
-        <Tabs
-          indicatorColor="#000000"
-          textColor="#000000"
-          onChange={handleChange}
-          aria-label="disabled tabs example"
-        >
+        <Tabs onChange={handleChange}>
           <Tab label="Criados" />
           <Tab label="Inscritos" />
         </Tabs>
       </Paper>
+      <div>
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <div>
         <NavBar />
       </div>
