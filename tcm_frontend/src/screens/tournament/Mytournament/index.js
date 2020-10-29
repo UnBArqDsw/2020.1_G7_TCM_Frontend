66e07f86a6505f2a63d81846c883ed440cb218dc
João Soares <jpcirqueira81@gmail.com>
2020-10-29 19:33:08 -0300

    aidicionado tabs a tela de meus torneios.

--- tcm_frontend/src/screens/tournament/Mytournament/index.js
+++ tcm_frontend/src/screens/tournament/Mytournament/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Paper from '@material-ui/core/Paper';
 import AppBar from '../../../components/Appbar';
 import NavBar from '../../../components/Navbar';
 
 const MyTournament = () => {
+  const handleChange = (event, newValue) => {
+    alert('teste');
+  };
+
   return (
     <Container>
       <AppBar />
+      <Paper>
+        <Tabs
+          indicatorColor="#000000"
+          textColor="#000000"
+          onChange={handleChange}
+          aria-label="disabled tabs example"
+        >
+          <Tab label="Criados" />
+          <Tab label="Inscritos" />
+        </Tabs>
+      </Paper>
       <div>
         <NavBar />
       </div>
