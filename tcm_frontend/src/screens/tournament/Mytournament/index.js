e266bcabdd1b22f4155a7993d8072d41375c71a7
João Soares <jpcirqueira81@gmail.com>
2020-11-07 19:07:07 -0300

    ajustado css tela meus torneios.

--- tcm_frontend/src/screens/tournament/Mytournament/index.js
+++ tcm_frontend/src/screens/tournament/Mytournament/index.js
@@ -2,27 +2,33 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Paper from '@material-ui/core/Paper';
 import AppBar from '../../../components/Appbar';
 import NavBar from '../../../components/Navbar';
 import Card from '../../../components/Card';
 import { useStyles } from './styles';
 
 const MyTournament = () => {
+  const [value, setValue] = React.useState(2);
   const styles = useStyles();
   const handleChange = (event, newValue) => {
-    alert('teste');
+    setValue(newValue);
+    // alert('teste');
   };
 
   return (
     <Container maxWidth="md" className={styles.styleDiv}>
       <AppBar />
-      <Paper>
-        <Tabs onChange={handleChange}>
-          <Tab label="Criados" />
-          <Tab label="Inscritos" />
-        </Tabs>
-      </Paper>
+      <Tabs
+        indicatorColor="#00000"
+        variant="scrollable"
+        scrollButtons="auto"
+        value={value}
+        onChange={handleChange}
+        className={styles.tabs}
+      >
+        <Tab label="Criados" />
+        <Tab label="Inscritos" />
+      </Tabs>
       <div>
         <Card />
         <Card />
--- tcm_frontend/src/screens/tournament/Mytournament/styles.js
+++ tcm_frontend/src/screens/tournament/Mytournament/styles.js
@@ -2,11 +2,15 @@ import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles((theme) => ({
   styleDiv: {
-    flexGrow: 1,
     height: '100%',
     margin: '0',
     padding: '0',
     alignItems: 'center',
     justify: 'flex-start',
+  },
+  tabs: {
+    backgroundColor: '#FF0000',
+    width: '50%',
+    marginLeft: '25%',
   },
 }));
