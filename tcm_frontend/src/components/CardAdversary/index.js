3c4a58200ef296b602c33ba90efa3bf18c4dfa4e
lucas <lucas.alexandre.df@gmail.com>
2020-11-10 10:45:03 -0300

    Ajustado erros na build
    Signed-off-by: lucas <lucas.alexandre.df@gmail.com>

--- tcm_frontend/src/components/CardAdversary/index.js
+++ tcm_frontend/src/components/CardAdversary/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Typography, Container } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import { useStyles } from './styles';
