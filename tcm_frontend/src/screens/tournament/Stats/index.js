3e90ee4be07de5b74b2afa307889eaa1284818aa
RCristyan <rcristyan9@gmail.com>
2020-11-13 15:28:05 -0300

    Adicionado componente de pontuação dos jogadores.

--- tcm_frontend/src/screens/tournament/Stats/index.js
+++ tcm_frontend/src/screens/tournament/Stats/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
+import ClearIcon from '@material-ui/icons/Clear';
 import { useStyles } from './style';
 import AppBar from '../../../components/Appbar';
 import Navbar from '../../../components/Navbar';
@@ -12,6 +13,22 @@ const Stats = () => {
       <AppBar />
       <div className={classes.centerBox}>
         <p className={classes.title}>Quartas de finais - BSB Open</p>
+        <div className={classes.scoreBox}>
+          <div className={classes.scoreBoxPlayers}>
+            <div className={classes.scoreBoxPlayer}>
+              <div className={classes.scoreBoxPlayerIcon} />
+              <p className={classes.scoreBoxPlayerName}>Moacir</p>
+              <p className={classes.scoreBoxPlayerName}>7</p>
+            </div>
+            <ClearIcon className={classes.scoreBoxPlayersSeparator} />
+            <div className={classes.scoreBoxPlayer}>
+              <div className={classes.scoreBoxPlayerIcon} />
+              <p className={classes.scoreBoxPlayerName}>Estanis</p>
+              <p className={classes.scoreBoxPlayerName}>5</p>
+            </div>
+          </div>
+          <p className={classes.scoreBoxSetSeparator}>Sets</p>
+        </div>
         <div className={classes.observationTable}>
           <p className={classes.observationTableTitle}>Observações</p>
           <div className={classes.observationBox}>
--- tcm_frontend/src/screens/tournament/Stats/style.js
+++ tcm_frontend/src/screens/tournament/Stats/style.js
@@ -20,6 +20,45 @@ export const useStyles = makeStyles(() => ({
     fontSize: '18px',
     marginTop: '20px',
   },
+  scoreBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '90%',
+    marginTop: '30px',
+    marginBottom: '80px',
+  },
+  scoreBoxPlayers: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  scoreBoxPlayersSeparator: {
+    height: '40px',
+    width: '40px',
+    marginBottom: '65px',
+  },
+  scoreBoxPlayer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  scoreBoxPlayerIcon: {
+    backgroundColor: '#EAEAEA',
+    height: '100px',
+    width: '100px',
+    borderRadius: '50px',
+    marginBottom: '15px',
+  },
+  scoreBoxPlayerName: {
+    fontSize: '18px',
+    marginTop: '10px',
+  },
+  scoreBoxSetSeparator: {
+    position: 'absolute',
+    left: '46%',
+    top: '43%',
+  },
   observationTable: {
     display: 'flex',
     flexDirection: 'column',
