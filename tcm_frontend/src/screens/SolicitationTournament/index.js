ccb051457cf40fb835157efeed0f9839e290c814
lucas <lucas.alexandre.df@gmail.com>
2020-11-17 00:08:47 -0300

    Adicionado textos na tela de solicitar torneio

--- tcm_frontend/src/screens/SolicitationTournament/index.js
+++ tcm_frontend/src/screens/SolicitationTournament/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Typography, Container } from '@material-ui/core';
 import AppBar from '../../components/Appbar';
 import NavBar from '../../components/Navbar';
 import { useStyles } from './style';
@@ -10,7 +10,16 @@ const SolicitationTournament = () => {
   return (
     <Container className={classes.container}>
       <AppBar />
-
+      <Typography className={classes.stylefont}>
+        Nome: Torneio Brasilia
+      </Typography>
+      <Typography className={classes.stylefont}>Estado: Brasilia</Typography>
+      <Typography className={classes.stylefont}>Cidade: Ceilandia</Typography>
+      <Typography className={classes.stylefont}>Inicio: 10/10/2020</Typography>
+      <Typography className={classes.stylefont}>Termino: 20/10/2020</Typography>
+      <Typography className={classes.stylefont}>
+        Número de Jogadores: 8
+      </Typography>
       <div className={classes.footer}>
         <NavBar />
       </div>
