7a90256d14872de27ae769618e69fd245fbd90cf
lucas <lucas.alexandre.df@gmail.com>
2020-11-18 12:19:49 -0300

    Adicionado menu item

--- tcm_frontend/src/components/Appbar/index.js
+++ tcm_frontend/src/components/Appbar/index.js
@@ -5,6 +5,9 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { MoreVert } from '@material-ui/icons';
 import IconButton from '@material-ui/core/IconButton';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -31,7 +34,21 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Appbar() {
   const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div className={classes.header}>
       <AppBar className={classes.appBar} position="fixed">
@@ -39,9 +56,33 @@ export default function Appbar() {
           <Typography variant="h6" className={classes.title}>
             TCM
           </Typography>
-          <IconButton>
-            <MoreVert className={classes.icon} />
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <AccountCircle className={classes.icon} />
           </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={open}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Perfil</MenuItem>
+            <MenuItem onClick={handleClose}>Sair</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </div>
