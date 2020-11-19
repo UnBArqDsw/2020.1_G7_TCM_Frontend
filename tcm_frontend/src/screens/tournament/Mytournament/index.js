773138a22b46910f37472f0e328bff0da4ab7db9
MoacirMSJ <moacirmascarenha86@gmail.com>
2020-11-19 14:14:35 -0300

    Adicionada logica para cards dinamicamente.

--- tcm_frontend/src/screens/tournament/Mytournament/index.js
+++ tcm_frontend/src/screens/tournament/Mytournament/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Container } from '@material-ui/core';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -7,13 +7,50 @@ import AppBar from '../../../components/Appbar';
 import NavBar from '../../../components/Navbar';
 import Card from '../../../components/Card';
 import { useStyles } from './styles';
+import api from '../../../services/api.js'
+
 
 const MyTournament = () => {
-  const [value, setValue] = React.useState(2);
   const styles = useStyles();
+  const [created_tournaments_list, setCreatedTournamentsList] = useState([])
+  const [participation_tournaments_list, setParticipationTournamentsList] = useState([])
+  const [value, setValue] = useState(0);
+  
+
+  useEffect(()=>{
+    async function getTournamnets(){
+      const response = await api.get('/tournament')
+      console.log(response.data)
+      if(response.status == 200){
+        setCreatedTournamentsList(response.data.tournament)
+      }
+
+      const response2 = await api.get('/user/tournaments');
+      if(response2.status == 200){
+        console.log(response2.data)
+        setParticipationTournamentsList(response2.data.solicitation)
+      }
+      
+    }
+    getTournamnets();
+  },[]);
+
+    
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  function a11yProps(index) {
+    return {
+      id: `full-width-tab-${index}`,
+      'aria-controls': `full-width-tabpanel-${index}`,
+    };
+  }
+
+  function handlerCard(id){
+    console.log('O link foi clicado.',id);
+    console.log(participation_tournaments_list)
+  }
 
   return (
     <Container maxWidth="md" className={styles.styleDiv}>
@@ -26,13 +63,31 @@ const MyTournament = () => {
         onChange={handleChange}
         className={styles.tabs}
       >
-        <Tab label="Criados" />
-        <Tab label="Inscritos" />
+        <Tab label="Criados" value={0} {...a11yProps(0)}/>
+        <Tab label="Inscritos" value={1} {...a11yProps(1)}/>
       </Tabs>
       <div>
-        <Card />
-        <Card />
-        <Card />
+        { value == 1 ? 
+          participation_tournaments_list.map(tournament =>(
+            <div key={tournament.tournaments.id} onClick={()=>handlerCard(tournament.tournaments.id)}>
+                <Card  
+                  nome={tournament.tournaments.name} 
+                  local={tournament.tournaments.endereco} 
+                  data={tournament.tournaments.start_date} 
+                />
+            </div>)
+            ) 
+            :
+            created_tournaments_list.map(tournament =>(
+              <div key={tournament.id} onClick={()=>handlerCard(tournament.id)}>
+                <Card  
+                  nome={tournament.name} 
+                  local={tournament.endereco} 
+                  data={tournament.start_date} 
+                />
+              </div>)
+            )
+        }
       </div>
       <div>
         <NavBar />
