453a72027b03c52f2d18cf1de7687725bff39527
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-11-19 15:39:08 -0300

    ajusta tela mytournaments

--- tcm_frontend/src/screens/tournament/Mytournament/index.js
+++ tcm_frontend/src/screens/tournament/Mytournament/index.js
@@ -1,98 +1,96 @@
-/* eslint-disable */
-import React, {useEffect, useState} from 'react';
-import { Container } from '@material-ui/core';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable camelcase */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-indent-props */
+import React, { useEffect, useState } from 'react';
+import { Container, Link } from '@material-ui/core';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import AppBar from '../../../components/Appbar';
 import NavBar from '../../../components/Navbar';
 import Card from '../../../components/Card';
 import { useStyles } from './styles';
-import api from '../../../services/api.js'
-
+import api from '../../../services/api';
 
 const MyTournament = () => {
   const styles = useStyles();
-  const [created_tournaments_list, setCreatedTournamentsList] = useState([])
-  const [participation_tournaments_list, setParticipationTournamentsList] = useState([])
+  const [createdList, dsetCreatedList] = useState([]);
+  const [participationList, setParticipationList] = useState([]);
   const [value, setValue] = useState(0);
-  
 
-  useEffect(()=>{
-    async function getTournamnets(){
-      const response = await api.get('/tournament')
-      console.log(response.data)
-      if(response.status == 200){
-        setCreatedTournamentsList(response.data.tournament)
+  useEffect(() => {
+    async function getTournamnets() {
+      const response = await api.get('/tournament');
+      if (response.status === 200) {
+        dsetCreatedList(response.data.tournament);
       }
 
       const response2 = await api.get('/user/tournaments');
-      if(response2.status == 200){
-        console.log(response2.data)
-        setParticipationTournamentsList(response2.data.solicitation)
+      if (response2.status === 200) {
+        setParticipationList(response2.data.solicitation);
       }
-      
     }
     getTournamnets();
-  },[]);
+  }, []);
 
-    
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  function a11yProps(index) {
-    return {
-      id: `full-width-tab-${index}`,
-      'aria-controls': `full-width-tabpanel-${index}`,
-    };
-  }
-
-  function handlerCard(id){
-    console.log('O link foi clicado.',id);
-    console.log(participation_tournaments_list)
-  }
+  const handleClick = () => {};
 
   return (
-    <Container maxWidth="md" className={styles.styleDiv}>
-      <AppBar />
-      <Tabs
-        indicatorColor="secondary"
-        variant="scrollable"
-        scrollButtons="auto"
-        value={value}
-        onChange={handleChange}
-        className={styles.tabs}
-      >
-        <Tab label="Criados" value={0} {...a11yProps(0)}/>
-        <Tab label="Inscritos" value={1} {...a11yProps(1)}/>
-      </Tabs>
-      <div>
-        { value == 1 ? 
-          participation_tournaments_list.map(tournament =>(
-            <div key={tournament.tournaments.id} onClick={()=>handlerCard(tournament.tournaments.id)}>
-                <Card  
-                  nome={tournament.tournaments.name} 
-                  local={tournament.tournaments.endereco} 
-                  data={tournament.tournaments.start_date} 
-                />
-            </div>)
-            ) 
-            :
-            created_tournaments_list.map(tournament =>(
-              <div key={tournament.id} onClick={()=>handlerCard(tournament.id)}>
-                <Card  
-                  nome={tournament.name} 
-                  local={tournament.endereco} 
-                  data={tournament.start_date} 
-                />
-              </div>)
-            )
-        }
-      </div>
-      <div>
-        <NavBar />
-      </div>
-    </Container>
+    <>
+      <Container maxWidth="md" className={styles.styleDiv}>
+        <AppBar />
+        <Tabs
+          indicatorColor="secondary"
+          variant="scrollable"
+          scrollButtons="auto"
+          value={value}
+          onChange={handleChange}
+          className={styles.tabs}
+        >
+          <Tab label="Criados" value={0} />
+          <Tab label="Inscritos" value={1} />
+        </Tabs>
+        {value === 1
+          ? participationList.map((tournament) => {
+              const {
+                id,
+                name,
+                endereco,
+                start_date,
+                cidade,
+              } = tournament.tournaments;
+              return (
+                <Link key={id} onClick={() => handleClick()}>
+                  <Card
+                    nome={name}
+                    cidade={cidade}
+                    local={endereco}
+                    data={start_date}
+                  />
+                </Link>
+              );
+            })
+          : createdList.map((tournament) => {
+              const { name, endereco, start_date, cidade, id } = tournament;
+              return (
+                <Link key={id} onClick={() => handleClick()}>
+                  <Card
+                    nome={name}
+                    cidade={cidade}
+                    local={endereco}
+                    data={start_date}
+                  />
+                </Link>
+              );
+            })}
+        <div className={styles.footer} />
+      </Container>
+      <NavBar />
+    </>
   );
 };
 
