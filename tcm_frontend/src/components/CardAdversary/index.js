a3ad3c0f88db738b49f589dc7f4bf849b95657f0
João Soares <jpcirqueira81@gmail.com>
2020-11-20 19:59:52 -0300

    Passando props para tela de match results.
    Signed-off-by: João Soares <jpcirqueira81@gmail.com>
    Co-authored-by: MoacirMSJ <moacirmascarenha86@gmail.com>

--- tcm_frontend/src/components/CardAdversary/index.js
+++ tcm_frontend/src/components/CardAdversary/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useEffect, useState } from 'react';
 import { Card } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 import Opponents from '../Opponents/index';
 import { useStyles } from './styles';
 import api from '../../services/api';
@@ -8,6 +11,7 @@ const CardAversary = () => {
   const classes = useStyles();
   const roundid = '11c44eda-60ff-49f5-bec6-e1f2725adcdd';
   const [matchs, setmatchs] = useState([]);
+  const history = useHistory();
   function getround() {
     api.get(`round/${roundid}`).then((res) => {
       setmatchs(res.data.matchs_list);
@@ -17,14 +21,23 @@ const CardAversary = () => {
   useEffect(() => {
     getround();
   }, []);
-  // console.log(matchs);
+
+  function handleOnClick(match) {
+    history.push({
+      pathname: '/match_result',
+      state: { match },
+    });
+  }
+
   return (
     <div>
       {matchs !== []
         ? matchs.map((match, id) => {
             return (
               <Card key={id} className={classes.card}>
-                <Opponents match={match} />
+                <div onClick={() => handleOnClick(match)}>
+                  <Opponents match={match} />
+                </div>
               </Card>
             );
           })
